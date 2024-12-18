2dad9d608e22d06d621b7746554c4946f3774069
diaba <fatoumata.diabate@ecoles-epsi.net>
2024-12-18 17:28:07 +0100

    rendu eval c#

--- Geometrie.DAL.Tests/Cercle_DALTests.cs
+++ Geometrie.DAL.Tests/Cercle_DALTests.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Geometrie.BLL.Tests
+namespace Geometrie.DAL.Tests
 {
-    internal class CercleTests
+    internal class Cercle_DALTests
     {
     }
 }
